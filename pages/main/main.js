03f956b91069961ccc4b076a6cb4fe6991393852
hqApril <forhyj@126.com>
2017-11-21 11:15:52 +0800

    hyj11/21 11:15
    miniapp init-2

---
+++ pages/main/main.js
@@ -0,0 +1,81 @@
+//main.js
+//获取应用实例
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "itemH": "200rpx"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    var _this = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        //console.log(res.screenWidth);
+        //console.log(res.screenHeight);
+        //console.log(res.windowWidth);
+        //console.log(res.windowHeight);
+        var avgW = (res.windowWidth-2)/3;
+        _this.setData({
+          "itemW": avgW + 'px',
+          "itemW_2": avgW*2 + 'px',
+          "itemH": avgW+ 15 + 'px'
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
