6b024bb66c709cb201970e61a83e24667ba65c57
hqApril <forhyj@126.com>
2017-11-28 17:42:21 +0800

    hyj 11/28 17:42
    complete dudu_user

--- pages/map/map.js
+++ pages/map/map.js
@@ -8,29 +8,15 @@ Page({
      */
     data: {
         itv: '',
-        markers: [{
-            iconPath: "../../imgs/marker.png",
-            id: 0,
-            latitude: 29.989643,
-            longitude: 116.481028,
-            width: 23,
-            height: 33
-        }, {
-            iconPath: "../../imgs/marker.png",
-            id: 0,
-            latitude: '',
-            longitude: '',
-            location: '',
-            width: 23,
-            height: 33
-        }],
-        distance: '',
+        markers: [],
+        distance: '司机离你还有 0 米',
         cost: '',
         polyline: [],
         textData: {},
         longitude: '',
         latitude: '',
-        location: ''
+        location: '',
+        inCarInfo: ''
     },
     makertap: function (e) {
         var id = e.markerId;
@@ -38,7 +24,6 @@ Page({
         that.showMarkerInfo(markersData, id);
         that.changeMarkerColor(markersData, id);
     },
-
     /**
      * 生命周期函数--监听页面加载
      */
@@ -55,10 +40,10 @@ Page({
             method: 'POST',
             success: function (res) {
                 var data = res.data;
-                console.log(data)
 
                 wx.setStorageSync('orderId', res.data);
 
+                that.paintMap();
                 that.data.itv = setInterval(that.paintMap, 2000);
             }
         })
@@ -90,11 +75,14 @@ Page({
             markers: markers
         });
     },
+
+    //调用wx接口 获取用户当前的实时信息
     getMyLocation: function () {
         var that = this;
 
         wx.getLocation({
             success: function (res) {
+
                 that.setData({
                     longitude: res.longitude,
                     latitude: res.latitude,
@@ -103,6 +91,8 @@ Page({
             }
         })
     },
+
+    //地图绘制
     paintMap() {
         var that = this;
 
@@ -125,27 +115,26 @@ Page({
                     that.setData({
                         itv: clearInterval(that.data.itv)
                     })
+
+                    that.inCar;
+                    that.data.itv = setInterval(that.inCar, 5000);
                 } else {
                     var driverLocation = data.dl_longitude + ',' + data.dl_latitude;
 
                     that.setData({
                         markers: [{
                             iconPath: "../../imgs/marker.png",
-                            id: 0,
-                            latitude: that.data.longitude,
-                            longitude: that.data.latitude,
+                            latitude: that.data.latitude,
+                            longitude: that.data.longitude,
                             width: 23,
                             height: 33
                         }, {
                             iconPath: "../../imgs/marker.png",
-                            id: 0,
-                            latitude: data.dl_longitude,
-                            longitude: data.dl_latitude,
-                            location: that.data.location,
+                            latitude: Number(data.dl_latitude),
+                            longitude: Number(data.dl_longitude),
                             width: 23,
                             height: 33
-                        }],
-                        distance: data.paths[0].distance
+                        }]
                     })
 
                     myAmapFun.getDrivingRoute({
@@ -171,14 +160,68 @@ Page({
                                     points: points,
                                     color: "#0091ff",
                                     width: 6
-                                }]
+                                }],
+                                distance: '司机离你还有 ' + data.paths[0].distance + ' 米'
                             });
                         }
                     })
                 }
-
-
             }
         })
+    },
+
+    //实时路径绘制
+    inCar: function () {
+        var that = this;
+
+        var data = {
+            orderId: wx.getStorageSync('orderId')
+        }
+
+        wx.request({
+            url: 'https://www.forhyj.cn/miniapp/User/getWayArr',
+            method: 'POST',
+            data: data,
+            success: function (res) {
+                var data = res.data;
+                var len = 0;
+
+                for (var i = 1; i < data.length; i++) {
+                    len += that.calculateDistance(data[i].latitude, data[i].longitude, data[i - 1].latitude, data[i - 1].longitude);
+                }
+
+                that.setData({
+                    polyline: [{
+                        points: data,
+                        color: "#0091ff",
+                        width: 6
+                    }],
+                    markers: [{
+                        iconPath: "../../imgs/marker.png",
+                        latitude: Number(data[data.length - 1].latitude),
+                        longitude: Number(data[data.length - 1].longitude),
+                        width: 23,
+                        height: 33
+                    }],
+                    distance: '已上车',
+                    inCarInfo: '路程： ' + len + ' km'
+                });
+            }
+        })
+    },
+
+    //根据经纬度算距离
+    calculateDistance: function (lat1, lng1, lat2, lng2) {
+        var radLat1 = lat1 * Math.PI / 180.0;
+        var radLat2 = lat2 * Math.PI / 180.0;
+
+        var a = radLat1 - radLat2;
+        var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+
+        var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+        s = s * 6378137.0;
+        s = Math.round(s * 10000) / 10000.0;
+
+        return s;
     }
 })
